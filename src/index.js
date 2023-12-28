2ff749cd9f68fd171064405f59cb594b35627fe2
Karol Faltyn <karolfaltyn03@gmail.com>
2023-12-28 22:26:45 +0100

    number generator done

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 
-
-
-
-
-const App = () => (
-  <div className='container'>
-    <div>
-      <h1>Countries List</h1>
+//generate div box from each number
+const Box_numbers = ({ numbers }) => {
+  const box = numbers.map((number) => <div className='item-box'><p>{number}</p></div>)
+  return (
+    <div className='flex-box'>
+      {box}
     </div>
-  </div>
-)
+  )
+};
 
 
+const Header = (props) => (
+  <header>
+    <h1>Number Generator</h1>
+    <p>Even are green, odds are yellow and prime are red.</p>
+  </header>
+);
 
+const Input = () => <input></input>
 
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+const App = () => {
+  const max_numbers = window.screen.height;
+  const numbers = [...Array(max_numbers).keys()];
+
+  // const numbers = [1, 2, 3, 4, 5];
+
+  return (
+    <div>
+      <Header numbers={numbers} />
+      <Input />
+      <Box_numbers numbers={numbers} />
+    </div>
+  );
+};
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
