1f4f4dea68b8f2032942c59c4691e49941980261
Karol Faltyn <karolfaltyn03@gmail.com>
2023-12-31 17:55:27 +0100

    New folder structure Divided components to smaller files and added folder hierarchy

---
+++ src/utils/isPrime.js
@@ -0,0 +1,10 @@
+export const isPrime = (num) => {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+
+    return true;
+};
